8d00a64a7d5d317263b0d9329bea9811992e6241
talahodaodethuong <htzhutaohub@gmail.com>
2025-12-18 21:54:59 +0700

    Add files via upload

---
+++ TongHopKichBan.lua
@@ -0,0 +1,40 @@
+local AllScript = {
+    [18687417158] = function()
+        loadstring(game:HttpGet(
+            "https://raw.githubusercontent.com/talahodaodethuong/Daylakichban/refs/heads/main/FSK-Free.lua"
+        ))()
+    end,
+
+    [126509999114328] = function()
+        loadstring(game:HttpGet(
+            "https://raw.githubusercontent.com/talahodaodethuong/Daylakichban/refs/heads/main/99NITF.lua"
+        ))()
+    end,
+
+    [2753915549] = function()
+        loadstring(game:HttpGet(
+            "https://raw.githubusercontent.com/htzgamingssr/Hutao-V3/refs/heads/main/Blox-Fruit-Script.lua"
+        ))()
+    end,
+
+    [4442272183] = function()
+        loadstring(game:HttpGet(
+            "https://raw.githubusercontent.com/htzgamingssr/Hutao-V3/refs/heads/main/Blox-Fruit-Script.lua"
+        ))()
+    end,
+
+    [7449423635] = function()
+        loadstring(game:HttpGet(
+            "https://raw.githubusercontent.com/htzgamingssr/Hutao-V3/refs/heads/main/Blox-Fruit-Script.lua"
+        ))()
+    end,
+}
+
+local currentPlaceId = game.PlaceId
+local runScript = AllScript[currentPlaceId]
+
+if runScript then
+    runScript()
+else
+    warn("This Script Is Not Supported This Game: " .. currentPlaceId)
+end
